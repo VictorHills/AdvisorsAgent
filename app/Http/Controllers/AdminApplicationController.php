42c394f4c201e3c57b45d82dca95340222ead421
victorhills <victorhills42@gmail.com>
2025-01-01 09:00:00 +0100

    Update admin application controller with new features

--- app/Http/Controllers/AdminApplicationController.php
+++ app/Http/Controllers/AdminApplicationController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ApplicationResource;
+use App\Mail\UpdateApplicationStatusEmail;
+use App\Models\ApplicationStatus;
 use App\Models\StudentApplications;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class AdminApplicationController extends Controller
 {
@@ -38,25 +44,54 @@ class AdminApplicationController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $application = StudentApplications::findOrFail($id);
+        return $this->respondSuccessWithData(message: 'Application created successfully', data: new ApplicationResource($application));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $application = StudentApplications::findOrFail($id);
+        $oldStatus = $application->status;
+        $application->update($request->all() + ['bdm_officer_id' => auth()->id()]);
+
+        $agent = User::where('id', $application->agent_id)->first();
+        $student = User::where('id', $application->student_id)->first();
+        $newStatus = ApplicationStatus::where('status_code', $request->status_name)->first();
+        $oldStatusName = ApplicationStatus::where('status_code', $oldStatus)->first();
+
+        try {
+            Mail::to($agent->email)->bcc([config('app.admin_email'), config('app.counselor_email')])
+                ->send(new UpdateApplicationStatusEmail(
+                    application: $application,
+                    oldStatus: $oldStatusName->status_name ?? 'Pending',
+                    newStatus: $newStatus->status_name ?? 'Pending',
+                ));
+        } catch (Exception $exception) {
+            Log::debug('Email could not be sent: ' . $exception->getMessage());
+        }
+
+
+        if ($request->status_name === 'conditional_offers_received' || $request->status_name === 'unconditional_offers_received') {
+            try {
+
+                Mail::to($student->email)->bcc([config('app.admin_email'), config('app.counselor_email'), auth()->user()->email])
+                    ->send(new UpdateApplicationStatusEmail(
+                        application: $application,
+                        oldStatus: $oldStatusName->status_name,
+                        newStatus: $newStatus->status_name,
+                    ));
+            } catch (Exception $exception) {
+                Log::debug('Email could not be sent: ' . $exception->getMessage());
+            }
+        }
+
+        return $this->respondSuccessWithData(message: 'Application updated successfully', data: new ApplicationResource($application));
     }
 
     /**
