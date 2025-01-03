d5b450f2d4f1d27371d9848d807b449fabd5a30b
victorhills <victorhills42@gmail.com>
2025-01-03 10:40:00 +0100

    Improve application status update email

--- app/Mail/UpdateApplicationStatusEmail.php
+++ app/Mail/UpdateApplicationStatusEmail.php
@@ -2,52 +2,40 @@
 
 namespace App\Mail;
 
+use App\Http\Resources\ApplicationResource;
+use App\Models\StudentApplications;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
 class UpdateApplicationStatusEmail extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+
+    public StudentApplications $application;
+    public string $oldStatus;
+    public string $newStatus;
 
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(StudentApplications $application, string $oldStatus, string $newStatus)
     {
-        //
+        $this->application = $application;
+        $this->oldStatus = $oldStatus;
+        $this->newStatus = $newStatus;
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
+    public function build(): UpdateApplicationStatusEmail
     {
-        return new Envelope(
-            subject: 'Update Application Status Email',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('Application Status Updated')
+            ->view('emails.updateApplicationStatusEmail')
+            ->with([
+                'application' => ApplicationResource::make($this->application),
+                'oldStatus' => $this->oldStatus,
+                'newStatus' => $this->newStatus,
+            ]);
     }
 }
