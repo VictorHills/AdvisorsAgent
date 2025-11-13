04519f400f1ff07cedf24f6df81a3e3cce116797
victorhills <victorhills42@gmail.com>
2025-11-13 15:30:00 +0100

    Configure app with new components and global settings

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
-import { createApp } from "vue"
+import {createApp} from "vue"
 import router from "./router/index.js"
 import App from "./components/App.vue"
 import "./bootstrap.js"
+
+const link = document.createElement("link")
+link.rel = "icon"
+link.type = "image/png"
+link.href = "/favicon.ico"
+document.head.appendChild(link)
 
 const app = createApp(App)
 
