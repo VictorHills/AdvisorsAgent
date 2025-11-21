a9e99181affa066128bab71f4aac0256486bc58b
victorhills <victorhills42@gmail.com>
2025-11-21 12:14:30 +0100

    Add phone field to users table, update student applications relationships, and enhance resource responses
    Signed-off-by: victorhills <victorhills42@gmail.com>

--- app/Http/Resources/ApplicationResource.php
+++ app/Http/Resources/ApplicationResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Countries;
+use App\Models\Universities;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,9 +17,26 @@ class ApplicationResource extends JsonResource
     public function toArray(Request $request): array
     {
         $response = parent::toArray($request);
-        $response['course'] = $this->course;
-        $response['agent'] = $this->agent;
+        $response['student'] = $this->student;
         $response['bdmOfficer'] = $this->bdmOfficer;
+
+        $response['course'] = [
+            'id' => $this->course->id ?? null,
+            'name' => $this->course->name ?? null,
+        ];
+
+        $response['agent'] = [
+            'id' => $this->agent->id ?? null,
+            'name' => $this->agent->first_name ?? null . ' ' . $this->agent->last_name ?? null,
+            'email' => $this->agent->email ?? null,
+            'phone' => $this->agent->phone ?? null,
+        ];
+
+        $response['schools_of_choice_details'] = Universities::whereIn('id', $this->schools_of_choice ?? [])
+            ->get(['id', 'name', 'logo']);
+
+        $response['country_of_preference_details'] = Countries::whereIn('id', $this->country_of_preference ?? [])
+            ->get(['id', 'name', 'currency']);
 
         return $response;
     }
