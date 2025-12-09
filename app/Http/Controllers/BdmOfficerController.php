9388429a258165198f1647f8b56b2bef608a5711
victorhills <victorhills42@gmail.com>
2025-12-09 16:00:27 +0100

    Remove unused bdmOfficer relationship from StudentApplications and User models
    Signed-off-by: victorhills <victorhills42@gmail.com>

--- app/Http/Controllers/BdmOfficerController.php
+++ app/Http/Controllers/BdmOfficerController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BusinessDevelopmentOfficers;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BdmOfficerController extends Controller
 {
     public function index(Request $request)
     {
+        $bdmOfficers = User::where('role_name', 'bdm-officer')
+            ->get(['id', 'first_name', 'last_name', 'email', 'phone']);
 
+        return $this->respondSuccessWithData('BDM Officers fetched successfully', $bdmOfficers);
     }
 
     public function show($id)
     {
+        $bdmOfficer = User::find($id);
+        if (is_null($bdmOfficer)) {
+            return $this->respondWithError("User does not exist");
+        }
+
+        return $this->respondSuccessWithData('BDM Officer fetched successfully', $bdmOfficer);
     }
 }
