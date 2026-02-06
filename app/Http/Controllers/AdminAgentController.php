989167e35d1544f55a7c55abe7cf6b3ca2040c73
victorhills <victorhills42@gmail.com>
2026-02-06 12:30:00 +0100

    refactor: Update AdminAgent controller

--- app/Http/Controllers/AdminAgentController.php
+++ app/Http/Controllers/AdminAgentController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContractRequest;
 use App\Http\Resources\AgentResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminAgentController extends Controller
 {
-    //
     public function index(Request $request)
     {
         $perPage = $request->get('per_page', 10);
@@ -26,5 +26,30 @@ class AdminAgentController extends Controller
             ->latest()->paginate($perPage);
 
         return AgentResource::collection($agents);
+    }
+
+    public function sendContract(ContractRequest $contractRequest)
+    {
+
+    }
+
+    public function confirmContractSent(ContractRequest $contractRequest)
+    {
+        $user = User::find($contractRequest->agent_id);
+
+        if (!$user->is_contract_sent) {
+            return $this->respondUnprocessed('Please send contract before confirming');
+        }
+
+        if ($user->is_contract_signed) {
+            return $this->respondSuccess('Contract already confirmed');
+        }
+
+        $user->update([
+            'is_contract_signed' => true,
+            'contract_signed_at' => now()
+        ]);
+
+        return $this->respondSuccess('Contract confirmed');
     }
 }
