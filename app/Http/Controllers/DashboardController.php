c835ef0cc6465c8f2f2a451e6330fffb8a87157f
VictorHills <victorhills42@gmail.com>
2026-04-17 15:03:02 +0100

    Updated Applications.vue

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,14 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Countries;
 use App\Models\StudentApplications;
 use App\Models\Students;
-use App\Traits\ApiResponses;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    use ApiResponses;
-
     public function stats()
     {
         $agentId = auth()->id();
